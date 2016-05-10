05904e102380e29882f6ab86fe2e5d37ea7fd2df
Игорь Францев < >
2016-05-11 02:43:53 +0600

    add map to detail and add page

--- app/js/element.client.controller.js
+++ app/js/element.client.controller.js
@@ -1,11 +1,33 @@
 'use strict';
 
 angular.module('addressBook')
+  .directive("regExInput", function(){
+    "use strict";
+    return {
+      restrict: "A",
+      require: "?regEx",
+      scope: {},
+      replace: false,
+      link: function(scope, element, attrs, ctrl){
+        element.bind('keypress', function (event) {
+          var regex = new RegExp(attrs.regEx);
+          var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+          if (!regex.test(key)) {
+            event.preventDefault();
+            return false;
+          }
+        });
+      }
+    };
+  })
+
+  // elements list page ctrl
+  //
   .controller('elementListCtrl', function ($localStorage,$state,ElementsStore) {
-    console.log('load');
     var vm = this;
     vm.elementsData = [];
 
+    //Load records
     ElementsStore.loadAll()
       .then(function (data) {
 
@@ -24,90 +46,130 @@ angular.module('addressBook')
 
     };
 
-
-
   })
-  .controller('elementDetailCtrl', function ($localStorage,$state,$stateParams,ElementsStore) {
+
+  // element detail view page ctrl
+  //
+  .controller('elementDetailCtrl', function ($localStorage,$state,$stateParams,ElementsStore,uiGmapGoogleMapApi,uiGmapIsReady) {
     var vm = this;
+
+    //Load records
     ElementsStore.loadAll();
-    console.log($stateParams);
+
     if ($stateParams.id) {
+      //Get record by id
       ElementsStore.get($stateParams.id)
         .then(function (data) {
-          console.log(data);
           vm.oneElement = data;
+          vm.mapCenter = { latitude:  +data.address.location.lat, longitude: +data.address.location.lng };
+          console.log(vm.mapCenter);
+
+          vm.map = {
+            center: vm.mapCenter,
+            zoom: 10
+          };
+
+          uiGmapIsReady.promise(1).then(function(instances) {
+            vm.mapInst = instances[0];
+            console.log(vm.mapInst);
+            //add marker to the map
+            new google.maps.Marker({
+              animation: google.maps.Animation.DROP,
+              position: data.address.location,
+              map:vm.mapInst.map
+            });
+          });
+
+          vm.setMapCenter = function () {
+            if (vm.mapInst) {
+              vm.mapInst.map.setCenter(data.address.location);
+            }
+          }
+
         });
+
+
+
     }
+    
+
 
   })
-  .controller('elementAddCtrl', function ($scope,$localStorage,$state,ElementsStore,uiGmapGoogleMapApi) {
+
+  // add element page ctrl
+  //
+  .controller('elementAddCtrl', function ($scope,$localStorage,$state,ElementsStore,uiGmapGoogleMapApi,uiGmapIsReady) {
 
     var vm = this;
+    var marker;
+    //Load records
+    ElementsStore.loadAll();
 
-    //{ latitude: 55.00597621361476, longitude: 82.9412841796875 } NSK coords
-    //(54.858417716801455, 83.10723781585693) AZOFT coords
-    uiGmapGoogleMapApi.then(function(maps) {
+    vm.map = {
+      center: {latitude: 55.00597621361476, longitude: 82.9412841796875},
+      zoom: 10,
+      mapEvents: {
+        'click': function (maps, eventName, originalEventArgs) {
+          var e = originalEventArgs[0];
 
-      var marker = new google.maps.Marker({
+          var geocoder = new google.maps.Geocoder();
+          var latlng = new google.maps.LatLng(e.latLng.lat(), e.latLng.lng());
+
+          geocoder.geocode({'latLng': latlng}, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+
+              if (results[0]) {
+                $scope.$apply(function () {
+                  vm.address = results[0].formatted_address;
+                  vm.latLng = latlng.toJSON();
+                });
+
+                console.log(results[0].formatted_address); // details address
+              } else {
+                console.log('Location not found');
+              }
+            } else {
+              console.log('Geocoder failed due to: ' + status);
+            }
+          });
+
+          marker.setPosition(latlng.toJSON());
+        }
+      }
+    };
+
+    uiGmapIsReady.promise(1).then(function(instances) {
+      var inst = instances[0];
+
+      vm.latLng = null;
+      marker = new google.maps.Marker({
         animation: google.maps.Animation.DROP,
+        map:inst.map
       });
 
-      vm.map = {
-        center: { latitude: 54.858417716801455, longitude: 83.10723781585693 },
-        zoom: 10,
-        mapEvents: {
-          'click': function (maps, eventName, originalEventArgs) {
-            var e = originalEventArgs[0];
-
-            var geocoder = new google.maps.Geocoder();
-            var latlng = new google.maps.LatLng(e.latLng.lat(), e.latLng.lng());
-
-            geocoder.geocode({ 'latLng': latlng }, function (results, status) {
-              if (status == google.maps.GeocoderStatus.OK) {
-
-                if (results[0]) {
-                  $scope.$apply(function () {
-                    vm.address = results[0].formatted_address;
-                  });
-
-                  console.log(results[0].formatted_address); // details address
-                } else {
-                  console.log('Location not found');
-                }
-              } else {
-                console.log('Geocoder failed due to: ' + status);
-              }
-            });
-
-
-            //console.log(maps, eventName, arg);
-            //console.log(arg[0].latLng.lat());
-            //console.log(arg[0].latLng.lng());
-            //console.log(arg[0].latLng.toJSON());
-            //console.log(arg[0].latLng.toString());
-
-            //latLngObj = arg[0].latLng;
-            marker.setPosition(latlng.toJSON());
-            marker.setMap(maps);
-          }
-        }
-      };
     });
 
-    vm.addRecord = function (valid) {
-      console.log(valid);
-      if (valid) {
+    vm.addRecord = function (formCtrl) {
+      console.log(formCtrl);
+      if (formCtrl.$valid) {
         vm.newElem = {
           add_time: Date.now(),
           name: vm.name,
-          add: vm.addition
+          address: {
+            formatted_address: vm.address,
+            location: vm.latLng
+          }
         };
         ElementsStore.add(generateRandomAlphaNum(8),vm.newElem);
-        vm.name = vm.addition = '';
+
+        vm.name = vm.address = '';
+        formCtrl.$setPristine();
+        formCtrl.$setUntouched();
       }
 
     };
 
+    //helper func to generate random ID
     function generateRandomAlphaNum(len) {
       var rdmString = "";
       for( ; rdmString.length < len; rdmString  += Math.random().toString(36).substr(2));
